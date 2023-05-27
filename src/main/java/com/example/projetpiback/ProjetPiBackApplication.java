68e55cc3a96a241dde8d3fb3660e5b581ed6b046
Eya124 <eya.trabelsi.1@esprit.tn>
2023-05-27 11:06:53 +0200

    first commit

---
+++ src/main/java/com/example/projetpiback/ProjetPiBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetpiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetpiback/ProjetPiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetpiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
