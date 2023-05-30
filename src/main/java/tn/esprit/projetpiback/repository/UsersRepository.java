b4c58aa669a0030ce8854f8afe35a47778cb5473
Eya124 <eya.trabelsi.1@esprit.tn>
2023-05-30 23:40:28 +0200

    change

--- src/main/java/tn/esprit/projetpiback/repository/UsersRepository.java
+++ src/main/java/tn/esprit/projetpiback/repository/UsersRepository.java
@@ -1,12 +1,13 @@
 package tn.esprit.projetpiback.repository;
 
+import org.springframework.stereotype.Repository;
 import tn.esprit.projetpiback.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
-
+@Repository
 public interface UsersRepository extends JpaRepository<User,Integer> {
     List<User> findAllByLastLog(LocalDate date);
     List<User> findAllByNomAndPrenom(String nom,String prenom);
